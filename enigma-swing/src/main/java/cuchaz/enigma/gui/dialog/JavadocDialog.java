1260f6322bc4d6357601a7c93e2fc1c15b23e685
Viktor VN <vannieuwenhuize.viktor@gmail.com>
2022-02-22 21:41:38 +0100

    compacted some code

--- enigma-swing/src/main/java/cuchaz/enigma/gui/dialog/JavadocDialog.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/dialog/JavadocDialog.java
@@ -209,41 +209,18 @@ public class JavadocDialog {
 
 	private class renderedJavadocWindowListener implements WindowListener {
 		@Override
-		public void windowOpened(WindowEvent e) {
-
-		}
-
-		@Override
 		public void windowClosing(WindowEvent e) {
 			if (renderedJavadocDialog != null) {
 				closeChildren();
 			}
 		}
 
-		@Override
-		public void windowClosed(WindowEvent e) {
-
-		}
-
-		@Override
-		public void windowIconified(WindowEvent e) {
-
-		}
-
-		@Override
-		public void windowDeiconified(WindowEvent e) {
-
-		}
-
-		@Override
-		public void windowActivated(WindowEvent e) {
-
-		}
-
-		@Override
-		public void windowDeactivated(WindowEvent e) {
-
-		}
+		@Override public void windowOpened(WindowEvent e) {}
+		@Override public void windowClosed(WindowEvent e) {}
+		@Override public void windowIconified(WindowEvent e) {}
+		@Override public void windowDeiconified(WindowEvent e) {}
+		@Override public void windowActivated(WindowEvent e) {}
+		@Override public void windowDeactivated(WindowEvent e) {}
 	}
 
 	private enum JavadocTag {
