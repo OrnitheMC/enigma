6d17d623fb5818060ce0a451bd4145b84f1f1da7
Space Walker <spacedoesrs@gmail.com>
2023-02-24 18:46:52 +0100

    fix field name validation

--- enigma/src/main/java/cuchaz/enigma/translation/representation/entry/FieldEntry.java
+++ enigma/src/main/java/cuchaz/enigma/translation/representation/entry/FieldEntry.java
@@ -89,7 +89,7 @@ public class FieldEntry extends ParentedEntry<ClassEntry> implements Comparable<
 
 	@Override
 	public boolean canConflictWith(Entry<?> entry, Predicate<Entry<?>> isStatic) {
-		return entry instanceof FieldEntry fieldEntry;
+		return entry instanceof FieldEntry fieldEntry && this.parent.equals(fieldEntry.parent);
 	}
 
 	@Override
